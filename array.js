9a957a277aa882e59ee479374a5198c663a52f02
Randall Gordon <rndllgrdn@icloud.com>
2019-03-22 21:57:58 -0400

    3/22

---
+++ array.js
@@ -0,0 +1,21 @@
+/*var arr = ['John','Nick','Bob','Michael','Mary']   // Array
+
+ for(var i = 0; i<5; i++){      // For Loop Incriment
+    console.log(arr[i]); */
+}
+
+// var colors = ['white','red','green','blue'];
+
+// var i = 0;
+/*
+while(i < colors.length) {        // While Loop checks perameters first
+    console.log(colors[i]);
+    i++
+}
+*/
+/*
+do {                              // Do While Loop Executes Code first
+  console.log(colors[i]);
+  i++
+} while(i < colors.length);
+*/
---
+++ date.js
@@ -0,0 +1,12 @@
+var date = new Date(); // DoW Month Day Year H:M:S TZ
+
+var year = date.getFullYear(); // 4 digit Year
+
+var month = date.getMonth(); // Month Jan = 0
+
+date.setFullYear(1990);  // set date
+date.setMonth(11); // set month
+
+console.log(date);
+console.log(year);
+console.log(month);
