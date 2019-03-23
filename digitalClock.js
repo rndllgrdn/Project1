c5fff03c075cd17dc431afb5c32380116fe98227
Randall Gordon <rndllgrdn@icloud.com>
2019-03-22 23:03:29 -0400

    Digital Clock

--- digitalClock.js
+++ digitalClock.js
@@ -1,1 +1,35 @@
-function digitalClock()
+function digitalClock() {
+
+    var date = new Date() + '';
+
+    var hours = date.getHours() + '';
+
+    var minutes = date.getMinutes() + '';
+
+    var seconds = date.getSeconds() + '';
+
+    var day = date.getDay();
+
+    if(hours.length < 2 ) {
+      hours = '0' + hours;
+    }
+
+    if(minutes.length < 2 ){
+      minutes = '0' + minutes;
+    }
+
+    if(seconds.length < 2 ) {
+      seconds  = '0' + seconds;
+    }
+    var weekDays = ['Sun' , 'Mon' , 'Tue' , 'Wed' , 'Thurs',
+                    'Fri' , 'Sat'];
+
+    var clock = weekDays[day] + ' ' + hours + ':' + minutes + ':' + seconds;
+
+    document.getElementById('clock').innerHTML = clock;
+
+}
+
+digitalClock();
+
+setInterval(digitalClock, 1000);
